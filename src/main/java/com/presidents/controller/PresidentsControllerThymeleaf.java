eae4d55b95a5d5bd4a834fc3c1940ecc32cb2111
Tomasz Wojtyra <t.wojtyra@il-pib.pl>
2022-09-04 16:50:52 +0200

    thymeleaf added

--- src/main/java/com/presidents/controller/PresidentsControllerThymeleaf.java
+++ src/main/java/com/presidents/controller/PresidentsControllerThymeleaf.java
@@ -1,19 +1,33 @@
 package com.presidents.controller;
 
+import com.presidents.model.dto.PresidentDto;
+import com.presidents.repository.PresidentsRepository;
+import com.presidents.service.president.PresidentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.Arrays;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequiredArgsConstructor
 public class PresidentsControllerThymeleaf {
 
+    private final PresidentsRepository presidentsRepository;
+    private final PresidentService presidentService;
+
     @GetMapping("/")
-    public String getIndex(Model model) {
-        model.addAttribute("name", "Tomek");
-        model.addAttribute("imiona", Arrays.asList("Karol", "Magda"));
+    public String getIndex(Model model, @RequestParam(name = "form", required = false, defaultValue = "false") Boolean form) {
+        model.addAttribute("presidents", presidentsRepository.findAll());
+        model.addAttribute("presidentDto", new PresidentDto());
+        model.addAttribute("form", form);
         return "index";
+    }
+
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String save(@ModelAttribute("presidentDto") PresidentDto presidentDto, Model model) {
+        presidentService.savePresident(presidentDto);
+        return "redirect:/";
     }
 }
