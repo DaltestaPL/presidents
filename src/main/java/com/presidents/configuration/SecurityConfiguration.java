02fa2d8dd48ba1341b4f1f121d902b17ad1ce44f
Tomasz Wojtyra <t.wojtyra@il-pib.pl>
2023-01-08 21:17:02 +0100

    security

--- src/main/java/com/presidents/configuration/SecurityConfiguration.java
+++ src/main/java/com/presidents/configuration/SecurityConfiguration.java
@@ -1,26 +1,13 @@
 package com.presidents.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-//    @Value("${username}")
-//    private String username;
-//
-//    @Value("${password}")
-//    private String password;
-//
-//    @Value("${role}")
-//    private String role;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -42,9 +29,4 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("admin").password("{noop}admin").roles("ADMIN");
     }
-
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
 }
